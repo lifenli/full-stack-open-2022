45ddd87527163d7a3c256c1261e9562f3b788dd7
lifen li <67099469+lifenli@users.noreply.github.com>
2022-07-21 16:12:03 +0700

    1.6: unicafe step1

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,39 +1,40 @@
-import React from "react";
-import Header from "./Header";
-import Part from "./Part";
-import Total from "./Total";
+import { useState } from 'react'
 
-
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-
-
+const Button = (props) => {
   return (
     <div>
-      <Header name={course.name} />
-      <Part part={course.parts[0]['name']} exercise={course.parts[0]['exercises']} />
-      <Part part={course.parts[1]['name']} exercise={course.parts[1]['exercises']} />
-      <Part part={course.parts[2]['name']} exercise={course.parts[2]['exercises']} />
-      <Total amount={course.parts[0]['exercises'] + course.parts[1]['exercises'] + course.parts[2]['exercises']} />
-    </div >
+      <button onClick={props.handleClick}>{props.text}</button>
+    </div>
+  )
+}
+const Stats = (props) => {
+  return (
+    <div>
+      {props.rating} {props.total}
+    </div>
   )
 }
 
 
-export default App;
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  // const [total, setTotal] = useState(0)
+
+  return (
+    <div>
+      <h1>Give Feeback</h1>
+      < Button handleClick={() => setGood(good + 1)} text="good" />
+      < Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      < Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <h2>Statistics</h2>
+      <Stats rating={'good'} total={good} />
+      <Stats rating={'neutral'} total={neutral} />
+      <Stats rating={'bad'} total={bad} />
+    </div>
+  )
+}
+
+export default App
